205dd333f6c32dacb92ef7d9020255a2e40ad62d
Max <sonthaya.nt@hotmail.com>
2022-03-28 20:42:19 +0700

    Update iKB1.py

--- modules/iKB1.py
+++ modules/iKB1.py
@@ -5,8 +5,10 @@ import os
 IKB_1_ADDR = 0x48
 
 machine = os.uname().machine
-if "KidBright32" in machine:
+if ("KidBright32" in machine) or ("KidMotor V4" in machine):
     i2c1 = I2C(1, scl=Pin(5), sda=Pin(4), freq=100000)
+elif "Mbits" in machine:
+    i2c1 = I2C(0, scl=Pin(21), sda=Pin(22), freq=100000)
 else:
     i2c1 = I2C(0, scl=Pin(22), sda=Pin(21), freq=100000)
 
